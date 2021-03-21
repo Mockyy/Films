41410f638df74ccc199dda619cf936af4cd10438
Mockyy <32643968+Mockyy@users.noreply.github.com>
2021-03-21 20:07:11 +0100

    Php

--- traitementHandXML.php
+++ traitementHandXML.php
@@ -1,19 +1,36 @@
 <?php
-header('content-type: text/XML');
+
+//header('content-type: text/xml');
 
 $tabEtudiant = array (
     array("nom" => "Mock", "prenom" => "Romain"),
-    array("nom" => "Sardou", "prenom" => "Michel")
+    array("nom" => "Sardou", "prenom" => "Michel"),
+    array("nom" => "Jean", "prenom" => "Damien"),
+    array("nom" => "Vincent", "prenom" => "Francky")
 );
 
 var_dump($tabEtudiant);
 
-$texte;
-*texte .= <Etudiants> //Root
+$txt = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 
-var_dump($texte);
+$txt .= "<Etudiants>\n";
 
-foreach ($tabEtudiant as $etudiant => $value) {
-    # code...
+foreach ($tabEtudiant as $etud) 
+{
+    $txt .= "\t<Etudiant>\n";
+
+    foreach($etud as $key => $val) 
+    {
+        $txt .= "\t<$key>". htmlspecialchars($val). "\t</$key>\n";
+    }
+        
+    $txt .= "\t</Etudiant>\n"; //Fin de l’étudiant
 }
+
+$txt .= "</Etudiants>\n"; //Fin des étudiant
+
+file_put_contents('Etudiants _txt.xml', $txt);
+
+//var_dump($txt);
+
 ?>
