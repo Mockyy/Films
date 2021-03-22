a470cf8cd677fc236e9f112ae1c70219cb70eab1
Mockyy <32643968+Mockyy@users.noreply.github.com>
2021-03-22 21:49:47 +0100

    Php XML et BDD

--- traitementHandXML.php
+++ traitementHandXML.php
@@ -11,6 +11,9 @@ $tabEtudiant = array (
 
 var_dump($tabEtudiant);
 
+
+///////Ecrire le XML à la main
+
 $txt = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 
 $txt .= "<Etudiants>\n";
@@ -32,5 +35,120 @@ $txt .= "</Etudiants>\n"; //Fin des étudiant
 file_put_contents('Etudiants _txt.xml', $txt);
 
 //var_dump($txt);
+
+
+
+/////XML WRITER
+
+$xml2= new XMLWriter();
+
+//Fichier xml de stockage des données
+
+$xml2->openUri("Etudiants_XMLWRITER.xml");
+
+$xml2->setIndent(true);
+
+$xml2->startDocument('1.0', 'utf-8');
+
+$xml2->startElement("Etudiants");
+
+//Boucler sur le tableau des étudiants avec $xml2->writeElement($balise,htmlspecialchars($valeur));
+foreach ($tabEtudiant as $etud) {
+
+    $xml2->startElement("Etudiant");
+
+    foreach ($etud as $key => $value) {
+        $xml2->writeElement($key, htmlspecialchars($value));
+    }
+    $xml2->endElement();
+}
+
+$xml2->endDocument();
+
+
+//////Dans le DOM
+$document = new DomDocument();
+
+$document->preserveWhiteSpace = false;
+
+$document->formatOutput = true;
+
+// on crée la racine <Etudiants> et on l'insère dans le document
+
+$lesEtudiants = $document->createElement('Etudiants');
+
+$document->appendChild($lesEtudiants);
+
+$i = 1;
+foreach ($tabEtudiant as $etud) {
+    
+    //Création d'un nouvel étudiant
+    $newEtud = $document->createElement('Etudiant');
+    $lesEtudiants->appendChild($newEtud);
+
+    //Ajout de l'attribut id 
+    $domAttribute = $document->createAttribute('id');
+    $domAttribute->value = $i;
+    $i++;
+    $newEtud->appendChild($domAttribute);
+  
+    //Création du nom et prénom d'un étudiant
+    foreach ($etud as $key => $value) {
+        $etud = $document->createElement($key, $value);
+        $newEtud->appendChild($etud);
+    }
+}
+
+$document->save('Etudiants_DOM.xml');
+
+echo "Export XML fini !";
+
+
+//////Charger un XML
+$lesEtudiantsXML = simplexml_load_file('Etudiants_DOM.xml');
+var_dump($lesEtudiantsXML);
+
+
+//////Connexion à la base de donnée
+$connexion = new mysqli('localhost', 'root', "", "clicom_simplifie");
+
+// Check connection
+if ($connexion->connect_error) {
+  die("Connection failed: " . $connexion->connect_error);
+}
+echo "Connected successfully";
+
+//Requête
+$sql = "SELECT * FROM client WHERE 1";
+$result = $connexion->query($sql);
+
+$documentClient = new DomDocument();
+$documentClient->preserveWhiteSpace = false;
+$documentClient->formatOutput = true;
+// on crée la racine <Clients> et on l'insère dans le document
+$lesClients = $documentClient->createElement('LesClients');
+$documentClient->appendChild($lesClients);
+
+//Affichage des résultats
+if ($result->num_rows > 0) {
+    //Affichage de chaque ligne
+    while($row = $result->fetch_assoc()) {
+        echo "NCLI : " . $row["NCLI"]. " - Nom : " . $row["NOM"]. " - Localite : " . $row["LOCALITE"]. "<br>";
+        
+        $newClient = $documentClient->createElement('Client');
+        $lesClients->appendChild($newClient);
+
+        foreach ($row as $key => $value) {
+            $newClientAttribute = $documentClient->createElement($key, $value);
+            $newClient->appendChild($newClientAttribute);
+        }
+    }
+} else {
+    echo "0 results";
+}
+
+$documentClient->save('clients.xml');
+
+$connexion->close();
 
 ?>
